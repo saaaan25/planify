a05736cf269ac1b635c4178c7345694dc0a4cc95
sofia <ssarmiento088@gmail.com>
2024-10-05 16:12:48 -0500

    changing space.jsx

--- frontend/src/pages/Space.jsx
+++ frontend/src/pages/Space.jsx
@@ -1,13 +1,11 @@
 // src/pages/Panel.jsx
 import React from 'react';
-import { useParams } from 'react-router-dom'; 
 
 const Space = () => {
-  const { id } = useParams();  // Obtenemos el ID del panel desde la URL
   return (
     <div>
-      <h1>Panel {id}</h1>
-      <p>Contenido del panel {id}.</p>
+      <h1>Panel</h1>
+      <p>Contenido del panel</p>
     </div>
   );
 }
